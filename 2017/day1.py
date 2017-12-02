1a9d40fdf49071de813e09ff468d83280a15496f
Sebastian Lange <sebastian.lange@lovoo.com>
2017-12-02 20:06:01 +0100

    day 1 riddle part 2

--- 2017/day1.py
+++ 2017/day1.py
@@ -1,13 +1,27 @@
 class Day1Puzzle:
-    def solve(input_numbers, step):
+    def solve(input_numbers):
         sum = 0
         last_number = None
         numbers_to_iterate = [int(i) for i in str(input_numbers)]
         numbers_to_iterate.append(numbers_to_iterate[0])
 
-        print("step: " + str(step))
-        for number in numbers_to_iterate[::step]:
+        for number in numbers_to_iterate:
             if number == last_number:
                 sum += number
             last_number = number
+        return sum
+
+    def solve_part_two(input_numbers):
+        numbers_to_iterate = [int(i) for i in str(input_numbers)]
+        number_of_items = len(numbers_to_iterate)
+        step = int(number_of_items / 2)
+
+        i = 0
+        sum = 0
+
+        while i <= number_of_items:
+            if i + step < number_of_items:
+                if numbers_to_iterate[i] == numbers_to_iterate[i + step]:
+                    sum += 2 * numbers_to_iterate[i]
+            i += 1
         return sum
