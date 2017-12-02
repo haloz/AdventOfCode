829cff68c2bfd2703cf7dd3bfd73af22a3e73450
Sebastian Lange <sebastian.lange@lovoo.com>
2017-12-02 21:18:10 +0100

    day 2 riddle part 2

--- 2017/day2.py
+++ 2017/day2.py
@@ -14,3 +14,17 @@ class Day2Puzzle:
                     max = element
             sum += max - min
         return sum
+
+    def handle_row_part_two(row_data):
+        for element in row_data:
+            for lookup_item in row_data:
+                if element != lookup_item and element % lookup_item == 0:
+                    return int(element / lookup_item)
+        return 0
+
+    def solve_part_two(data):
+        sum = 0
+        for row in data:
+            sum += Day2Puzzle.handle_row_part_two(row)
+
+        return sum
